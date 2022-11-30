5e8e15c91711908fe39d881853b457c784205ded
Laura Izquierdo <laurairodrigo@gmail.com>
2022-11-30 10:00:31 +0100

    cambio messaggio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("prova2");
+        System.out.println("cambio prova2");
     }
 }
