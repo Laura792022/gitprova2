5fc0c5d68ab11fcf03c216585a1cc668b5fd3e90
Luca Coraci <luca.coraci@gmail.com>
2022-12-02 11:19:57 +0100

    more things

--- src/main/java/es/laura/Main.java
+++ src/main/java/es/laura/Main.java
@@ -2,14 +2,14 @@ package es.laura;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("cambio prova 3");
-        System.out.println("dovrei essere qui");
+        System.out.println("[DEVELOPER BRANCH] cambio prova 3");
+        System.out.println("[DEVELOPER BRANCH] dovrei essere qui");
         Person p = new Person();
         p.setSurname("Izquierdo");
         p.setName("Luca");
 
-        System.out.println(">   Name: "+p.getName());
-        System.out.println("> Surname: "+p.getSurname());
+        System.out.println("[DEVELOPER BRANCH] >   Name: "+p.getName());
+        System.out.println("[DEVELOPER BRANCH] > Surname: "+p.getSurname());
 
         System.out.println("il nome e' lungo "+p.getName().length() + " caratteri") ;
         System.out.println("The end");
